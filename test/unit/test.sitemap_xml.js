078b7e817aaa18a3a64a0883bdba444711fc47af
Ryan Betts <ryan@ishuman.co>
2019-07-17 17:42:01 -0700

    updated @wdio + eslint packages, and fixed broken tests

--- test/unit/test.sitemap_xml.js
+++ test/unit/test.sitemap_xml.js
@@ -9,7 +9,7 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-/* global describe, it */
+
 const assert = require('assert');
 const remark = require('remark');
 const defaultMain = require('../../src-actual/helix/sitemap_xml.js');
--- test/unit/test.summary_html.pre.js
+++ test/unit/test.summary_html.pre.js
@@ -9,7 +9,7 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-/* global describe, it */
+
 const assert = require('assert');
 const defaultPre = require('../../src-actual/helix/summary_html.pre.js');
 
